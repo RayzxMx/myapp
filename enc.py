6b4c901b57fe1aa1705dcbfa7b1a620c23827984
Amatsume <antaakbar.05@gmail.com>
2024-07-02 18:40:57 +0700

    enc 5

--- enc.py
+++ enc.py
@@ -12,9 +12,9 @@ def get_image_download_link(img, filename, text):
     img_str = base64.b64encode(buffered.getvalue()).decode()
     href = f'<a href="data:image/png;base64,{img_str}" download="{filename}">{text}</a>'
     return href
+
 #Fungsi untuk menghitung kapasitas penyimpanan gambar cover
-def calculate_capacity(image_path):
-    image = Image.open(image_path)
+def calculate_capacity(image):
     if image.mode == 'RGBA':
         image = image.convert('RGB')
     width, height = image.size
@@ -23,15 +23,14 @@ def calculate_capacity(image_path):
     return width, height, total_capacity_bits, total_capacity_bytes
 
 # Fungsi untuk menyesuaikan ukuran gambar hidden agar tidak melebihi ukuran gambar cover
-def resize_image(input_image_path, output_image_path, new_width, new_height):
-    image = Image.open(input_image_path)
+def resize_image(image, new_width, new_height):
     if image.mode == 'RGBA':
         image = image.convert('RGB')
     resized_image = image.resize((new_width, new_height))
-    return new_width, new_height
+    return resized_image, new_width, new_height
 
-def image_to_base64(image_path):
-    encoded_string = base64.b64encode(image_path.read()).decode('utf-8')
+def image_to_base64(image):
+    encoded_string = base64.b64encode(image.read()).decode('utf-8')
     return encoded_string
 
 def base64_to_image(encoded_string, output_image_path):
@@ -64,32 +63,60 @@ def encryptPage():
     # Unggah gambar cover
     st.markdown("<h4 style='text-align: left;'>Upload Gambar Cover</h4>", unsafe_allow_html=True)
     cover_file = st.file_uploader('', type=['png', 'jpg', 'bmp'], key="cover")
+    
     if cover_file is not None:
+        #Buka gambar cover
         cover = Image.open(cover_file)
+        #ubah ke RGB jika gambar RGBA
         if cover.mode == 'RGBA':
             cover = cover.convert('RGB')
+        #Nilai pixel dari cover
         cover_pixels = np.array(cover)
+        #Mengambil nilai tinggi dan lebar cover
         cover_height, cover_width, _ = cover_pixels.shape
-
+        #Menghitung total kapasitas gambar cover
+        cover_width, cover_height, total_capacity_bits, total_capacity_bytes = calculate_capacity(cover)
         # Unggah gambar pesan
         st.markdown("<h4 style='text-align: left;'>Upload File</h4>", unsafe_allow_html=True)
         message_file = st.file_uploader('', type=['png', 'jpg', 'bmp' , 'pdf'], key="message")
-        combined_binary = ''
         if message_file is not None:
+            #Deklarasi variabel kombinasi data biner
+            combined_binary = ''
+
+            #Cek tipe file
             if message_file.type == 'application/pdf':
+                #Tipe file PDF
                 file_type = 'P'
+                #Ubah pdf ke base64
                 pdf_base64 = pdf_to_base64(message_file)
+                #combinasi data biner
                 combined_binary = '0' + string_to_binary(file_type) + string_to_binary(pdf_base64) + '1111111111111110'  # End of message delimiter
             else:
+                #File tipe Gambar
                 file_type = 'G'
+                #Gambar hidden
                 hidden_image = Image.open(message_file)
+                #Ubah ke RGB jika gambar RGBA
                 if hidden_image.mode == 'RGBA':
                     hidden_image = hidden_image.convert('RGB')
-                    hidden_base64 = image_to_base64(message_file)
-                    hidden_width, hidden_height = hidden_image.size
-                    hidden_width_binary = int_to_binary_string(hidden_width, 16)
-                    hidden_height_binary = int_to_binary_string(hidden_height, 16)
-                    combined_binary = '0' + string_to_binary(file_type) + hidden_width_binary + hidden_height_binary + string_to_binary(hidden_base64) + '1111111111111110'  # End of message delimiter
+                #Ambil data panjang, lebar dan aspek rasio dari gambar hidden
+                hidden_width, hidden_height = hidden_image.size
+                hidden_aspect_ratio = hidden_width / hidden_height
+
+                #Hitung nilai maksimum piksel hidden
+                max_hidden_pixels = total_capacity_bits // 24 // 4
+                new_hidden_width = int((max_hidden_pixels * hidden_aspect_ratio) ** 0.5)
+                new_hidden_height = int(new_hidden_width / hidden_aspect_ratio)
+
+                #Ubah ukuran gambar hidden
+                resized_image, _, _ = resize_image(hidden_image, new_hidden_width, new_hidden_height)
+
+                #Ubah Gambar ke base64
+                hidden_base64 = image_to_base64(resized_image)
+
+                hidden_width_binary = int_to_binary_string(new_hidden_width, 16)
+                hidden_height_binary = int_to_binary_string(new_hidden_height, 16)
+                combined_binary = '0' + string_to_binary(file_type) + hidden_width_binary + hidden_height_binary + string_to_binary(hidden_base64) + '1111111111111110'  # End of message delimiter
                     
             # Ensure the hidden data can fit within the cover image
             if len(combined_binary) > cover_height * cover_width * 3:
