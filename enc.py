0a8917736d421e48c629d31ce42d66de97799a3b
Amatsume <antaakbar.05@gmail.com>
2024-07-02 19:08:37 +0700

    a

--- enc.py
+++ enc.py
@@ -30,8 +30,9 @@ def resize_image(image, new_width, new_height):
     return resized_image
 
 def image_to_base64(image):
-    image_bytes = image.read()  # Read the file as bytes
-    encoded_string = base64.b64encode(image_bytes).decode('utf-8')
+    buffered = BytesIO()
+    image.save(buffered, format='PNG')
+    encoded_string = base64.b64encode(buffered.getvalue()).decode('utf-8')
     return encoded_string
 
 def base64_to_image(encoded_string, output_image_path):
