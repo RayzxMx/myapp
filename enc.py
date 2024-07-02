a186d434cdff4befcb61134b062dcf95da9d46a2
Amatsume <antaakbar.05@gmail.com>
2024-07-02 22:02:01 +0700

    a

--- enc.py
+++ enc.py
@@ -23,16 +23,17 @@ def calculate_capacity(image):
     return width, height, total_capacity_bits, total_capacity_bytes
 
 # Fungsi untuk menyesuaikan ukuran gambar hidden agar tidak melebihi ukuran gambar cover
-def resize_image(image, new_width, new_height):
+def resize_image(input_image_path, output_image_path, new_width, new_height):
+    image = Image.open(input_image_path)
     if image.mode == 'RGBA':
         image = image.convert('RGB')
     resized_image = image.resize((new_width, new_height))
-    return resized_image
+    resized_image.save(output_image_path)
+    return new_width, new_height
 
-def image_to_base64(image):
-    buffered = BytesIO()
-    image.save(buffered, format='PNG')
-    encoded_string = base64.b64encode(buffered.getvalue()).decode('utf-8')
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
     return encoded_string
 
 def base64_to_image(encoded_string, output_image_path):
@@ -60,6 +61,61 @@ def binary_to_string(binary_data):
     chars = [binary_data[i:i+8] for i in range(0, len(binary_data), 8)]
     return ''.join(chr(int(char, 2)) for char in chars)
 
+def encode_image(cover_image_path, input_file_path, output_image_path):
+    cover_image = Image.open(cover_image_path)
+    if cover_image.mode == 'RGBA':
+        cover_image = cover_image.convert('RGB')
+
+    cover_pixels = np.array(cover_image)
+    cover_height, cover_width, _ = cover_pixels.shape
+
+    file_extension = os.path.splitext(input_file_path)[1].lower()
+    
+    if file_extension in ['.png', '.jpg', '.jpeg', '.bmp', '.gif']:
+        file_type = 'G'
+        hidden_image = Image.open(input_file_path)
+        if hidden_image.mode == 'RGBA':
+            hidden_image = hidden_image.convert('RGB')
+        hidden_base64 = image_to_base64(input_file_path)
+        
+        hidden_width, hidden_height = hidden_image.size
+        hidden_width_binary = int_to_binary_string(hidden_width, 16)
+        hidden_height_binary = int_to_binary_string(hidden_height, 16)
+        combined_binary = '0' + string_to_binary(file_type) + hidden_width_binary + hidden_height_binary + string_to_binary(hidden_base64) + '1111111111111110'  # End of message delimiter
+
+    elif file_extension == '.pdf':
+        file_type = 'P'
+        pdf_base64 = pdf_to_base64(input_file_path)
+        combined_binary = '0' + string_to_binary(file_type) + string_to_binary(pdf_base64) + '1111111111111110'  # End of message delimiter
+
+    else:
+        raise ValueError("Tipe file tidak didukung. Gunakan gambar dengan ekstensi .png, .jpg, .jpeg, .bmp, .gif atau PDF dengan ekstensi .pdf.")
+
+    # Ensure the hidden data can fit within the cover image
+    if len(combined_binary) > cover_height * cover_width * 3:
+        raise ValueError("Ukuran data tersembunyi terlalu besar untuk disisipkan ke dalam gambar cover.")
+
+    data_index = 0
+    binary_message_length = len(combined_binary)
+
+    for y in range(cover_height):
+        for x in range(cover_width):
+            if data_index < binary_message_length:
+                r, g, b = cover_pixels[y, x][:3]
+
+                r = (r & ~1) | int(combined_binary[data_index])
+                data_index += 1
+                if data_index < binary_message_length:
+                    g = (g & ~1) | int(combined_binary[data_index])
+                    data_index += 1
+                if data_index < binary_message_length:
+                    b = (b & ~1) | int(combined_binary[data_index])
+                    data_index += 1
+
+                cover_pixels[y, x] = [r, g, b]
+    return cover_pixels
+    
+
 # Fungsi enkripsi gambar
 def encryptPage():
     # Unggah gambar cover
@@ -67,88 +123,32 @@ def encryptPage():
     cover_file = st.file_uploader('', type=['png', 'jpg', 'bmp'], key="cover")
     
     if cover_file is not None:
-        #Buka gambar cover
-        cover = Image.open(cover_file)
-        #ubah ke RGB jika gambar RGBA
-        if cover.mode == 'RGBA':
-            cover = cover.convert('RGB')
-        #Nilai pixel dari cover
-        cover_pixels = np.array(cover)
-        #Mengambil nilai tinggi dan lebar cover
-        cover_height, cover_width, _ = cover_pixels.shape
-        #Menghitung total kapasitas gambar cover
-        cover_width, cover_height, total_capacity_bits, total_capacity_bytes = calculate_capacity(cover)
+        cover_image_path = cover_file
         # Unggah gambar pesan
         st.markdown("<h4 style='text-align: left;'>Upload File</h4>", unsafe_allow_html=True)
         message_file = st.file_uploader('', type=['png', 'jpg', 'bmp' , 'pdf'], key="message")
         if message_file is not None:
-            #Deklarasi variabel kombinasi data biner
-            combined_binary = ''
-            encode_pixels = ''
-            #Cek tipe file
-            if message_file.type == 'application/pdf':
-                #Tipe file PDF
-                file_type = 'P'
-                #Ubah pdf ke base64
-                pdf_base64 = pdf_to_base64(message_file)
-                #combinasi data biner
-                combined_binary = '0' + string_to_binary(file_type) + string_to_binary(pdf_base64) + '1111111111111110'  # End of message delimiter
-            else:
-                #File tipe Gambar
-                file_type = 'G'
-                #Gambar hidden
-                hidden_image = Image.open(message_file)
-                #Ubah ke RGB jika gambar RGBA
-                if hidden_image.mode == 'RGBA':
-                    hidden_image = hidden_image.convert('RGB')
-                #Ambil data panjang, lebar dan aspek rasio dari gambar hidden
-                hidden_width, hidden_height = hidden_image.size
-                hidden_aspect_ratio = hidden_width / hidden_height
+            input_file_path = message_file
+            cover_width, cover_height, total_capacity_bits, total_capacity_bytes = calculate_capacity(cover_image_path)
+            hidden_image = Image.open(input_file_path)
+            if hidden_image.mode == 'RGBA':
+                hidden_image = hidden_image.convert('RGB')
+            hidden_width, hidden_height = hidden_image.size
+            hidden_aspect_ratio = hidden_width / hidden_height
 
-                #Hitung nilai maksimum piksel hidden
-                max_hidden_pixels = total_capacity_bits // 24 // 4
-                new_hidden_width = int((max_hidden_pixels * hidden_aspect_ratio) ** 0.5)
-                new_hidden_height = int(new_hidden_width / hidden_aspect_ratio)
+            max_hidden_pixels = total_capacity_bits // 24 // 4
+            new_hidden_width = int((max_hidden_pixels * hidden_aspect_ratio) ** 0.5)
+            new_hidden_height = int(new_hidden_width / hidden_aspect_ratio)
 
-                #Ubah ukuran gambar hidden
-                resized_image = resize_image(hidden_image, new_hidden_width, new_hidden_height)
+            resized_hidden_image_path = 'resized_hidden_image.png'
+            resize_image(input_file_path, resized_hidden_image_path, new_hidden_width, new_hidden_height)
+            input_file_path = resized_hidden_image_path
 
-                #Ubah Gambar ke base64
-                hidden_base64 = image_to_base64(resized_image)
-
-                hidden_width_binary = int_to_binary_string(new_hidden_width, 16)
-                hidden_height_binary = int_to_binary_string(new_hidden_height, 16)
-                combined_binary = '0' + string_to_binary(file_type) + hidden_width_binary + hidden_height_binary + string_to_binary(hidden_base64) + '1111111111111110'  # End of message delimiter
-                    
-            # Ensure the hidden data can fit within the cover image
-            if len(combined_binary) > cover_height * cover_width * 3:
-                st.write("Ukuran data tersembunyi terlalu besar untuk disisipkan ke dalam gambar cover.")
-            data_index = 0
-            binary_message_length = len(combined_binary)
-
-            for y in range(cover_height):
-                for x in range(cover_width):
-                    if data_index < binary_message_length:
-                        r, g, b = cover_pixels[y, x][:3]
-
-                        r = (r & ~1) | int(combined_binary[data_index])
-                        data_index += 1
-                        if data_index < binary_message_length:
-                            g = (g & ~1) | int(combined_binary[data_index])
-                            data_index += 1
-                        if data_index < binary_message_length:
-                            b = (b & ~1) | int(combined_binary[data_index])
-                            data_index += 1
-
-                        cover_pixels[y, x] = [r, g, b]
-
+            cover_pixels = encode_image(cover_image_path, input_file_path, 'encoded_image.png')
+            encoded_image = Image.fromarray(cover_pixels)
+            encoded_image.save('stego.png')
             # Tampilkan gambar stego
             st.image(cover_pixels, caption='This is your stego image', channels='GRAY')
 
-            # Ubah kembali array stego menjadi gambar
-            stego_img = Image.fromarray(cover_pixels)
-
-            stego_img.save('stego.png')
-
             # Tambahkan link unduhan
-            st.markdown(get_image_download_link(stego_img, 'stego.png', 'Download Stego Image'), unsafe_allow_html=True)
+            st.markdown(get_image_download_link(encoded_image, 'stego.png', 'Download Stego Image'), unsafe_allow_html=True)
